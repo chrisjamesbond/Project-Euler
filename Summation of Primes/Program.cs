a137fc4c5778fe5ed2b17fac064b85434780fae4
chrisjamesbond <chrisjamesbond@gmail.com>
2014-12-08 03:10:58 -0500

    solved summation of primes

---
+++ Summation of Primes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summation_of_Primes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumPrimesToN(2000000));
+            Console.Read();
+        }
+
+        public static double SumPrimesToN(double n)
+        {
+            double SumOfPrimes = 0;
+            for (double i = 3; i < n; i += 2)
+            {
+                if (IsPrime(i))
+                    SumOfPrimes+= i;
+            }
+
+            return SumOfPrimes + 2;
+        }
+
+        public static bool IsPrime(double n)
+        {
+            if (n <= 1 || n == 9)
+                return false;
+
+            if (n == 2)
+                return true;
+
+            if (n % 2 == 0)
+                return false;
+
+            for (double i = 3; i * i <= n; i += 2)
+                if (n % i == 0)
+                    return false;
+
+            return true;
+        }
+    }
+}

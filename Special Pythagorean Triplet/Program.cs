f1b60197372e26bc6b0f5b8079ed0b85b56806e3
chrisjamesbond <chrisjamesbond@gmail.com>
2014-12-08 03:27:49 -0500

    solved special pythagorean triplet

--- Special Pythagorean Triplet/Program.cs
+++ Special Pythagorean Triplet/Program.cs
@@ -10,8 +10,44 @@ namespace Special_Pythagorean_Triplet
     {
         static void Main(string[] args)
         {
+            Console.Write("product = " + FindSpecialPythagoreanTriplet(1000));
+            Console.Read();
+            
         }
 
-        
+        public static double FindSpecialPythagoreanTriplet(double n)
+        {
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = i + 1; j <= n - i; j++)
+                {
+                    for (int k = j + 1; k <= n - (j + i); k++)
+                    {
+                        if (CheckForRightTriangle(i,j,k) && (i + j + k) == n)
+                        {
+                            Console.WriteLine("a = " + i);
+                            Console.WriteLine("b = " + j);
+                            Console.WriteLine("c = " + k);
+                            return i * j * k;
+                        }
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        public static bool CheckForRightTriangle(double a, double b, double c)
+        {
+            double LenghOfSidesSquared = (a * a) + (b * b);
+            double LengthOfHypotenuseSquared = c * c;
+
+            if (LenghOfSidesSquared == LengthOfHypotenuseSquared)
+                return true;
+
+            return false;
+        }
     }
 }
+
+
